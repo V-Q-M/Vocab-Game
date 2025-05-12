5450e627ae28d50dc15bbf4c3a0306557f605a41
V-Q-M <vr-mueller@protonmail.com>
2025-05-12 23:28:19 +0200

    update

--- main.py
+++ main.py
@@ -431,7 +431,7 @@ def handle_move(player, objects):
 
 
 def load_words():
-    questions = ["привет", "спасибо", "да", "нет", "извините", "как", "кто", "что", "где"]
+    questions = ["привет", "спасибо", "да", "нет", "как", "кто", "что", "где"]
     answers = ["hallo", "danke", "ja", "nein", "bitte", "wie", "wer", "was", "wo"]
     return questions,answers
 
